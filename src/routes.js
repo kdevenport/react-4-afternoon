47bb05cf8899b88e8b4178120e9cd6b6bee7155a
kdevenport <katie.l.devenport@gmail.com>
2019-03-13 14:39:01 -0600

    stepfourcomplete

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import ClassList from './components/ClassList/ClassList';
 
 export default (
     <Switch>
         <Route exact path = '/' component = {Home} />
         <Route exact path = '/about' component = {About} />
+        <Route exact path = '/classlist/:class' component = {ClassList} />
     </Switch>
 )
